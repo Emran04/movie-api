692e8160f21cf66fd142c629e58056b3ea0ddd91
Emran04 <emran04ch@gmail.com>
2022-10-03 23:49:54 +0600

    seeder and readme file updated

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Movie;
+use App\Models\Plan;
+use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,12 +18,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $movie    = Movie::factory()->create([
+            'plan' => Movie::PLAN_PREMIUM,
+        ]);
 
-        \App\Models\User::factory()->create([
-            'name'  => 'Emran Hossen',
-            'email' => 'emran@app.com',
+        Movie::factory()->create([
+            'plan' => Movie::PLAN_BASIC,
+        ]);
+
+        $customer1 = User::factory()->create([
+            'name'  => 'Premium',
+            'email' => 'premium@customer.com',
             'type'  => User::TYPE_CUSTOMER,
+        ]);
+
+        Plan::factory()->create([
+            'user_id' => $customer1->id,
+            'type'    => Plan::TYPE_PREMIUM,
+            'from'    => now()->subDays(2),
+            'to'      => now()->addDays(30),
+        ]);
+
+        $customer2 = User::factory()->create([
+            'name'  => 'Basic',
+            'email' => 'basic@customer.com',
+            'type'  => User::TYPE_CUSTOMER,
+        ]);
+
+        Plan::factory()->create([
+            'user_id' => $customer2->id,
+            'type'    => Plan::TYPE_BASIC,
+            'from'    => now()->subDays(2),
+            'to'      => now()->addDays(30),
+        ]);
+
+        Subscription::factory()->create([
+            'user_id' => $customer2->id,
+            'movie_id' => $movie->id,
+            'from'    => now()->subDays(2),
+            'to'      => now()->addDays(30),
+        ]);
+
+        // Admin
+        User::factory()->create([
+            'name'  => 'Admin',
+            'email' => 'admin@app.com',
+            'type'  => User::TYPE_ADMIN,
         ]);
     }
 }
